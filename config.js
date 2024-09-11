3617769e2351993b69e946e565eac40a02a4561a
Muhammad Hussain <Muhammadhussainsiddqi@gmail.com>
2024-09-12 00:09:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+// Load the environment variables from the .env file
+dotenv.config();
 export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL = process.env.REACT_APP_API_KEY;

2f5d93fb357d6d26673be935ce795c7308824796
Muhammad Hussain <Muhammadhussainsiddqi@gmail.com>
2024-09-11 22:16:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL =
   'mongodb+srv://hussain:mh6302288@cluster0.ti747.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
